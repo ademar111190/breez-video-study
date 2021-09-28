e9c8ab0bd0a051aa6725e632a9ff8d060cadc190
Ademar Alves de Oliveira <ademar111190@gmail.com>
2021-09-28 10:01:12 -0300

    Adding initial stuff

---
+++ lib/consts.dart
@@ -0,0 +1,1 @@
+const URL = 'https://noagendatube.us-east-1.linodeobjects.com/videos/0d26b52c-4c93-4030-a4d8-809d896c45ee-1080.mp4';
---
+++ lib/home.dart
@@ -0,0 +1,163 @@
+import 'dart:io';
+
+import 'package:breezvideo/consts.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var _moment = Moment.INITIAL;
+  var _errorMsg = "";
+  var _downloadMsg = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Breez Video'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.clear_all),
+            tooltip: 'Clear',
+            onPressed: () {
+              _setMoment(Moment.INITIAL);
+            },
+          ),
+        ],
+      ),
+      body: _body(),
+    );
+  }
+
+  Widget _body() {
+    switch (_moment) {
+      case Moment.INITIAL:
+        return _bodyInitial();
+      case Moment.DOWNLOADING:
+        return _bodyDownloading();
+      case Moment.ERROR:
+        return _bodyError();
+    }
+  }
+
+  Widget _bodyInitial() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+          child: ElevatedButton(
+            child: Text('Start download'),
+            onPressed: () {
+              _download();
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _bodyDownloading() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+          child: Text(_downloadMsg),
+        ),
+      ],
+    );
+  }
+
+  Widget _bodyError() {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text('Error: $_errorMsg.'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _setMoment(Moment newMoment) {
+    setState(() {
+      if (mounted) {
+        _moment = newMoment;
+      }
+    });
+  }
+
+  void _download() async {
+    _downloadMsg = 'Downloading…';
+    _setMoment(Moment.DOWNLOADING);
+
+    try {
+      final taskId = await FlutterDownloader.enqueue(
+        url: URL,
+        savedDir: await _getStorageDirectory(),
+        showNotification: true,
+        openFileFromNotification: true,
+      );
+    } catch (e) {
+      _errorMsg = e.toString();
+      _setMoment(Moment.ERROR);
+    }
+    FlutterDownloader.registerCallback(_downloadCallback);
+  }
+
+  void _downloadCallback(String id, DownloadTaskStatus status, int progress) {
+    switch (status.value) {
+      case 1: // enqueued
+        _downloadMsg = 'Download requested';
+        _setMoment(Moment.DOWNLOADING);
+        break;
+      case 2: // running
+        _downloadMsg = 'Downloading $progress%';
+        _setMoment(Moment.DOWNLOADING);
+        break;
+      case 3: // complete
+        _downloadMsg = 'Download completed';
+        _setMoment(Moment.DOWNLOADING);
+        break;
+      case 4: // failed
+        _downloadMsg = 'Download failed';
+        _setMoment(Moment.DOWNLOADING);
+        break;
+      case 5: // canceled
+        _downloadMsg = 'Download canceled';
+        _setMoment(Moment.DOWNLOADING);
+        break;
+      case 6: // paused
+        _downloadMsg = 'Download pause';
+        _setMoment(Moment.DOWNLOADING);
+        break;
+      default:
+        _downloadMsg = 'Downloading ???';
+        _setMoment(Moment.DOWNLOADING);
+        break;
+    }
+  }
+
+  Future<String> _getStorageDirectory() async {
+    Directory directory = await getApplicationDocumentsDirectory();
+    final path = join(directory.path, 'BreezVideo');
+    await Directory(path).create(recursive: true);
+    return path;
+  }
+}
+
+enum Moment {
+  INITIAL,
+  DOWNLOADING,
+  ERROR,
+}
